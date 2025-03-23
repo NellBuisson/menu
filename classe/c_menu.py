2f76daf90b562c0065db6f1b4b47c11d6c7be6f6
Nell Buisson <nell.buisson@outlook.fr>
2025-03-23 01:48:09 +0100

    fin (normalement lol) de la classe Menu. On vera plus tard comment on fait avec liste ingredient et liste de course hummm et plat. Peut-être faire une classe mère ingrédient (utilisé dans la classe Plats) et les ingrédients en kg où a l'unité. En vrai, je pense que je devrais séparé ses deux classes et faire un comparatif après ? hummmm à voir

--- classe/c_menu.py
+++ classe/c_menu.py
@@ -2,43 +2,65 @@ from c_menujour import MenuJour
 
 class Menu : 
     def __init__(self, nbr):
-        self.menusJour = []
+        self.listePlats = []
         self.nbrPlats = 0
         self.nbrTotalRepas = nbr
         
     def AjouterPlat(self, j, p) :
-        nouvMenu = MenuJour(j, p)
 
-        #On gère la semaine du nouveau plat ajouté
-        if self.nbrPlats != 0 :
-            for plat in self.menusJour :
-                if plat.Date() == nouvMenu.Date() :
-                    nouvMenu.ModifierSemaine(nouvMenu.Semaine() + 1)
+        if self.nbrPlats != self.nbrTotalRepas :
+            nouvMenu = MenuJour(j, p)
 
-        self.menusJour.append(nouvMenu)
-        self.nbrTotalRepas += 1
+            #On gère la semaine du nouveau plat ajouté
+            if self.nbrPlats != 0 :
+                for plat in self.listePlats :
+                    if plat.Date() == nouvMenu.Date() :
+                        nouvMenu.ModifierSemaine(nouvMenu.Semaine() + 1)
+
+            #on ajoute le nouveau menu au Menu
+            self.listePlats.append(nouvMenu)
+            self.nbrPlats += 1
+
     def AjouterPlatPers(self, j, p, nbr) :
-        nouvMenu = MenuJour(j, p)
+        if self.nbrPlats != self.nbrTotalRepas :
+            nouvMenu = MenuJour(j, p)
 
-        #On gère la semaine du nouveau plat ajouté
-        if self.nbrPlats != 0 :
-            for plat in self.menusJour :
-                if plat.Date() == nouvMenu.Date() :
-                    nouvMenu.ModifierSemaine(nouvMenu.Semaine() + 1)
+            #On gère la semaine du nouveau plat ajouté
+            if self.nbrPlats != 0 :
+                for plat in self.listePlats :
+                    if plat.Date() == nouvMenu.Date() :
+                        nouvMenu.ModifierSemaine(nouvMenu.Semaine() + 1)
 
-        #On gère le changement de nombre de personnes
-        nouvMenu.ModifierNbrPersonne(nbr)
+            #On gère le changement de nombre de personnes
+            nouvMenu.ModifierNbrPersonne(nbr)
 
-
-        self.menusJour.append(nouvMenu)
-        self.nbrTotalRepas += 1
+            #on ajoute le nouveau menu au Menu
+            self.listePlats.append(nouvMenu)
+            self.nbrPlats += 1
 
     def RetirerPlat(self, plat) : 
-        for menu in self.menusJour : 
+        for menu in self.listePlats : 
             if menu.Plat() == plat.lower() : 
-                self.menusJour.remove(menu)
+                self.listePlats.remove(menu)   
+                self.nbrPlats -= 1        
 
-    def ListePlats(self) :
-        return self.menusJour
+    def Plats(self) :
+        return self.listePlats
+
+    def PlatManquant(self) :
+        return self.nbrTotalRepas - self.nbrPlats
+    
+    def NbrPlats (self) :
+        return self.nbrPlats
+    
+    def ModifierNbrRepas(self, nbr) :
+        if self.NbrPlats() <= nbr :
+            self.nbrTotalRepas = nbr 
+    
+    def Afficher(self) :
+        print(f"Menu pour {self.nbrTotalRepas} repas, possède actuellement {self.nbrPlats} :")
+
+        for plat in self.listePlats :
+            plat.Afficher()
 
 
