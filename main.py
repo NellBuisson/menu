c8caef2d8f8f6800de4503d09a03fac024ef898a
Nell Buisson <nell.buisson@outlook.fr>
2025-03-27 14:29:13 +0100

    pb dans man. fonction (commentaire) trop d'itérable ? Trouver comment faire individuellement

--- main.py
+++ main.py
@@ -3,24 +3,30 @@ from classe.c_menu import *
 from classe.c_listeplat import *
 
 
-def RemplirCourseAvecMenu(menu, liste_de_course, liste_plat) :
-    for plat in menu.Plats() :
+def RemplirCourseAvecMenu(menu, liste_de_course, liste_plat) :   
+    for repas in menu.ListeRepas() :
+        plat, nbr_pers = repas.Repas()
+        
         for ingredient_et_quantite in liste_plat.IngredientsPlat(plat) :
+            
+            print(ingredient_et_quantite)
+            
             for ingredient, quantite in ingredient_et_quantite :
+                if nbr_pers != 2 :
+                    quantite = nbr_pers*quantite/2
                 liste_de_course.AjouterElement(ingredient, quantite)
 
     liste_de_course.Afficher()
 
 menu_semaine = Menu(4)
-menu_semaine.AjouterPlat("Dimanche", "pate carbo")
-menu_semaine.AjouterPlat("Samedi", "poulet")
-menu_semaine.AjouterPlat("Lundi", "Riz au thon")
+menu_semaine.AjouterRepas("Dimanche", "pate carbo")
+menu_semaine.AjouterRepas("Samedi", "poulet")
 
 liste_plat = ListePlats()
 liste_plat.AjouterPlat("pate carbo", {"pate" : 0.3, "sauce" : 1, "lardon" : 1})
-liste_plat.AjouterPlat("pate carbo", {"poulet" : 1, "sauce" : 1})
-
+liste_plat.AjouterPlat("poulet", {"poulet" : 1, "sauce" : 1})
+liste_plat.AjouterPlat("Riz au thon", {"thon" : 1, "tomate" : 1})
 liste_de_course = ListeCourse("auj")
 
-RemplirCourseAvecMenu(menu_semaine, liste_de_course, liste_plat)
 
+RemplirCourseAvecMenu(menu_semaine, liste_de_course, liste_plat)
